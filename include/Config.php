6d2a8b9c963ec761f7c0114b746961057a256911
Panfilo <Panfilo@Panfi>
2017-10-09 20:16:44 -0500

    avance del app movil

---
+++ include/Config.php
@@ -0,0 +1,10 @@
+<?php
+ 
+/**
+ * Configuracion de variables de la base de datos
+ */
+define("DB_HOST", "localhost");
+define("DB_USER", "root");
+define("DB_PASSWORD", "root");
+define("DB_DATABASE", "android_api");
+?>
